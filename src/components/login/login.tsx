b8b5d76701b1c3a9010aa9b9557b52d14a4787eb
Samhitha <samhitha@quoqo.com>
2024-02-27 15:28:23 +0530

    az-error configuration

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,27 +1,23 @@
 "use client";
 import { AI_NAME } from "@/features/theme/customise";
-import { signIn } from "next-auth/react";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 export const LogIn = () => {
-  const handleAiIconClick = () => {
-    // Trigger sign-in when AI icon is clicked
-    signIn("azure-ad");
-  };
+  // Remove the handleAiIconClick function
 
   return (
     <Card className="flex gap-2 flex-col min-w-[300px]">
       <CardHeader className="gap-2">
         <CardTitle className="text-2xl flex gap-2">
-          {/* Attach onClick event directly to the Avatar component */}
-          <Avatar className="h-14 w-14" onClick={handleAiIconClick}>
+          {/* Remove the onClick event from the Avatar component */}
+          <Avatar className="h-14 w-14">
             <AvatarImage src={"ai-icon.png"} />
           </Avatar>
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
-        <CardDescription >
-          
+        <CardDescription>
+          {/* You can keep or modify the CardDescription content as needed */}
         </CardDescription>
       </CardHeader>
       {/* Remove the Microsoft 365 button */}
