a06561cf9ef81b183dfb26de55a4348f3561df1a
Quoqo-repo <98815631+Quoqo-repo@users.noreply.github.com>
2024-02-28 16:34:57 +0530

    back to original code

--- src/features/auth/helpers.ts
+++ src/features/auth/helpers.ts
@@ -1,52 +1,8 @@
-// import { createHash } from "crypto";
-// import { getServerSession } from "next-auth";
-// import { options } from "./auth-api";
-
-// export const userSession = async (): Promise<UserModel | null> => {
-//   const session = await getServerSession(options);
-//   if (session && session.user) {
-//     return session.user as UserModel;
-//   }
-
-//   return null;
-// };
-
-// export const userHashedId = async (): Promise<string> => {
-//   const user = await userSession();
-//   if (user) {
-//     return hashValue(user.email);
-//   }
-
-//   throw new Error("User not found");
-// };
-
-// export type UserModel = {
-//   name: string;
-//   image: string;
-//   email: string;
-// };
-
-// export const hashValue = (value: string): string => {
-//   const hash = createHash("sha256");
-//   hash.update(value);
-//   return hash.digest("hex");
-// };
-
-
 import { createHash } from "crypto";
 import { getServerSession } from "next-auth";
 import { options } from "./auth-api";
 
 export const userSession = async (): Promise<UserModel | null> => {
-  if (process.env.NODE_ENV === 'development') {
-    // During development, return a mock user
-    return {
-        name: 'mock-user',
-        image: 'mock-image',
-        email: 'mock-email'
-    };
-  }
-
   const session = await getServerSession(options);
   if (session && session.user) {
     return session.user as UserModel;
@@ -56,11 +12,6 @@ export const userSession = async (): Promise<UserModel | null> => {
 };
 
 export const userHashedId = async (): Promise<string> => {
-  if (process.env.NODE_ENV === 'development') {
-    // During development, return a mock hashed id/user id
-    return 'mock-hashed-id';
-  }
-  
   const user = await userSession();
   if (user) {
     return hashValue(user.email);
@@ -80,3 +31,52 @@ export const hashValue = (value: string): string => {
   hash.update(value);
   return hash.digest("hex");
 };
+
+
+// import { createHash } from "crypto";
+// import { getServerSession } from "next-auth";
+// import { options } from "./auth-api";
+
+// export const userSession = async (): Promise<UserModel | null> => {
+//   if (process.env.NODE_ENV === 'development') {
+//     // During development, return a mock user
+//     return {
+//         name: 'mock-user',
+//         image: 'mock-image',
+//         email: 'mock-email'
+//     };
+//   }
+
+//   const session = await getServerSession(options);
+//   if (session && session.user) {
+//     return session.user as UserModel;
+//   }
+
+//   return null;
+// };
+
+// export const userHashedId = async (): Promise<string> => {
+//   if (process.env.NODE_ENV === 'development') {
+//     // During development, return a mock hashed id/user id
+//     return 'mock-hashed-id';
+//   }
+  
+//   const user = await userSession();
+//   if (user) {
+//     return hashValue(user.email);
+//   }
+
+//   throw new Error("User not found");
+// };
+
+// export type UserModel = {
+//   name: string;
+//   image: string;
+//   email: string;
+// };
+
+// export const hashValue = (value: string): string => {
+//   const hash = createHash("sha256");
+//   hash.update(value);
+//   return hash.digest("hex");
+// };
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,44 +5,7 @@ const requireAuth: string[] = ["/chat", "/api","/reporting", "/unauthorized"];
 const requireAdmin: string[] = ["/reporting"];
 
 
-// export async function middleware(request: NextRequest) {
-
-//     const res = NextResponse.next();
-//     const pathname = request.nextUrl.pathname;
-
-//     if (requireAuth.some((path) => pathname.startsWith(path))) {
-
-//         const token = await getToken({
-//             req: request
-//         });
-
-//         //check not logged in
-//         if (!token) {
-//             const url = new URL(`/`, request.url);
-//             return NextResponse.redirect(url);
-//         }
-
-//         if (requireAdmin.some((path) => pathname.startsWith(path))) {
-//             //check if not authorized
-//             if (!token.isAdmin) {
-//                 const url = new URL(`/unauthorized`, request.url);
-//                 return NextResponse.rewrite(url);
-//             }
-//         }
-//     }
-
-//     return res;
-// }
-
-// // note that middleware is not applied to api/auth as this is required to logon (i.e. requires anon access)
-// export const config = { matcher: ["/chat/:path*", "/reporting/:path*", "/api/chat:path*"] };
-
-
 export async function middleware(request: NextRequest) {
-
-    if (process.env.NODE_ENV === 'development') {
-        return NextResponse.next(); // Continue to the next middleware/request if running in development mode
-    }
 
     const res = NextResponse.next();
     const pathname = request.nextUrl.pathname;
@@ -53,14 +16,14 @@ export async function middleware(request: NextRequest) {
             req: request
         });
 
-        // Check if user is not logged in
+        //check not logged in
         if (!token) {
             const url = new URL(`/`, request.url);
             return NextResponse.redirect(url);
         }
 
         if (requireAdmin.some((path) => pathname.startsWith(path))) {
-            // Check if the user is not an admin
+            //check if not authorized
             if (!token.isAdmin) {
                 const url = new URL(`/unauthorized`, request.url);
                 return NextResponse.rewrite(url);
@@ -71,4 +34,41 @@ export async function middleware(request: NextRequest) {
     return res;
 }
 
+// note that middleware is not applied to api/auth as this is required to logon (i.e. requires anon access)
 export const config = { matcher: ["/chat/:path*", "/reporting/:path*", "/api/chat:path*"] };
+
+
+// export async function middleware(request: NextRequest) {
+
+//     if (process.env.NODE_ENV === 'development') {
+//         return NextResponse.next(); // Continue to the next middleware/request if running in development mode
+//     }
+
+//     const res = NextResponse.next();
+//     const pathname = request.nextUrl.pathname;
+
+//     if (requireAuth.some((path) => pathname.startsWith(path))) {
+
+//         const token = await getToken({
+//             req: request
+//         });
+
+//         // Check if user is not logged in
+//         if (!token) {
+//             const url = new URL(`/`, request.url);
+//             return NextResponse.redirect(url);
+//         }
+
+//         if (requireAdmin.some((path) => pathname.startsWith(path))) {
+//             // Check if the user is not an admin
+//             if (!token.isAdmin) {
+//                 const url = new URL(`/unauthorized`, request.url);
+//                 return NextResponse.rewrite(url);
+//             }
+//         }
+//     }
+
+//     return res;
+// }
+
+// export const config = { matcher: ["/chat/:path*", "/reporting/:path*", "/api/chat:path*"] };
